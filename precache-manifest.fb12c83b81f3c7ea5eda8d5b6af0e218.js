df2971c28add71f4974595146d943f3ad2a272bf
c-h-o-c-o-l-a-t-e <chaos.mayhem.blood@gmail.com>
2020-08-29 19:57:11 +0530

    Updates

---
+++ precache-manifest.fb12c83b81f3c7ea5eda8d5b6af0e218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5da4eaa608c75af51f15fbaf301e773",
+    "url": "/calculator-js/index.html"
+  },
+  {
+    "revision": "6faba8ed8a2d4949cc46",
+    "url": "/calculator-js/static/css/main.9fe64de1.chunk.css"
+  },
+  {
+    "revision": "0a639420a1682567d1bc",
+    "url": "/calculator-js/static/js/2.5ada497e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-js/static/js/2.5ada497e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6faba8ed8a2d4949cc46",
+    "url": "/calculator-js/static/js/main.2be1eb35.chunk.js"
+  },
+  {
+    "revision": "e849a2773d3d1e32bf63",
+    "url": "/calculator-js/static/js/runtime-main.10541860.js"
+  }
+]);
